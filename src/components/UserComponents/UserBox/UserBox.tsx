0aab5349060f87a8ee473a16e4c09b8e58c6639c
HanninenF <hanninenfredrik@gmail.com>
2025-02-24 16:56:50 +0100

    add: resetInput function

--- src/components/UserComponents/UserBox/UserBox.tsx
+++ src/components/UserComponents/UserBox/UserBox.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import Button from '../../Buttons/button';
-import UserCard from '../../UserComponents/UserCard/UserCard';
-import './User.scss';
+import { useState, useEffect } from "react";
+import Button from "../../Buttons/button";
+import UserCard from "../../UserComponents/UserCard/UserCard";
+import "./User.scss";
 
 export type User = {
   name: string;
@@ -9,12 +9,20 @@ export type User = {
 };
 
 export default function UserBox() {
-  const [user, setUser] = useState<User>({ name: '', age: 0 });
+  const [user, setUser] = useState<User>({ name: "", age: 0 });
   const [users, setUsers] = useState<User[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: name === "age" ? Number(value) : value,
+    }));
   };
+  //logga state
+  useEffect(() => {
+    console.log("Users updated:", user);
+  }, [user]);
 
   const handleDelete = (index: number) => {
     setUsers(users.filter((_, i) => i !== index));
@@ -24,38 +32,60 @@ export default function UserBox() {
     setUsers([]);
   };
 
+  //resettar inputen med hjälp av nyckel. Om nyckeln age hämtas sätts värdet till 0 annars tom sträng
+  const resetInput = (key: keyof User) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      [key]: key === "age" ? 0 : "",
+    }));
+  };
+
   return (
-    <div className='userBox'>
+    <div className="userBox">
       <p>User</p>
-      <label>
-        Name:
-        <input
-          type='text'
-          name='name'
-          value={user.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Age:{' '}
-        <input
-          type='number'
-          name='age'
-          value={user.age}
-          onChange={handleChange}
-        />{' '}
-      </label>
+      <div className="inputWrapper">
+        <label htmlFor="name">Name:</label>
+
+        <div className="inputContainer">
+          <input
+            className="nameInput"
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <button className="resetNameInput" onClick={() => resetInput("name")}>
+            X
+          </button>
+        </div>
+      </div>
+      <div className="inputWrapper">
+        <label htmlFor="age">Age:</label>
+
+        <div className="inputContainer">
+          <input
+            className="ageInput"
+            type="number"
+            name="age"
+            value={user.age}
+            onChange={handleChange}
+          />
+          <button className="resetAgeInput" onClick={() => resetInput("age")}>
+            X
+          </button>
+        </div>
+      </div>
       <Button
-        title='Submit'
+        title="Submit"
         handleClick={() => {
           if (user.name && user.age) {
             setUsers([...users, user]);
-            setUser({ name: '', age: 0 });
+            setUser({ name: "", age: 0 });
           }
         }}
       />
-      <div className='userContainer'>
-        {' '}
+      <div className="userContainer">
+        {" "}
         {users.map((user, index) => (
           <UserCard
             key={`${index}-${user.name}`}
@@ -65,7 +95,7 @@ export default function UserBox() {
         ))}
       </div>
       {users.length > 1 && (
-        <Button title='Delete all' handleClick={() => deleteAll()} />
+        <Button title="Delete all" handleClick={() => deleteAll()} />
       )}
     </div>
   );
